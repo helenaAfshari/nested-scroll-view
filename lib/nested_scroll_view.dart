6d26d1775837ab369ee70d756941895b4d01c7c5
Hana <afshari.hln@gmail.com>
2022-11-25 19:13:04 +0330

    Create NestedScroolView

---
+++ lib/nested_scroll_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NestedScrollVieww extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    body: Scaffold(
+        body: DefaultTabController(
+          length: 1,
+          child: NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) => [
+              SliverAppBar(
+                title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const[
+                      Text("NestedScroll", style:TextStyle(color: Color.fromARGB(255, 211, 145, 145)) ),
+                     
+                    ]),
+                backgroundColor: Colors.black54,
+                pinned: true,
+                floating: true,
+                expandedHeight: 100,
+                bottom: const TabBar(
+                  tabs: [
+                  Icon(Icons.camera_alt_outlined),
+                   Icon(Icons.camera_alt_outlined),
+                    Icon(Icons.camera_alt_outlined),                 
+                  ],
+                ),
+              ),
+            ], body: SingleChildScrollView(
+                 
+            ),
+           
+          ),
+        ),
+      
+      ),
+   );
+   
+  }
+
+}
